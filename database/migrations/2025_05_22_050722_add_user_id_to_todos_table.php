0887406e9b326cc6495d328d88d5defc8fddeaf2
Tasya Zahrani <tasyaa23@mhs.usk.ac.id>
2025-05-22 12:11:45 +0700

    edit by tasyaa

---
+++ database/migrations/2025_05_22_050722_add_user_id_to_todos_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+{
+    Schema::table('todos', function (Blueprint $table) {
+        if (!Schema::hasColumn('todos', 'user_id')) {
+    $table->foreignId('user_id')->constrained()->onDelete('cascade');
+}
+    });
+}
+
+    public function down()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+    }
+};
